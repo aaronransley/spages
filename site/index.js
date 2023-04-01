84b812abac7d330b5c4ab11e82d908fad9d77355
Aaron Ransley <aaronransley@gmail.com>
2023-03-31 19:26:29 -0600

    lol app instance maybe

--- site/index.js
+++ site/index.js
@@ -1,24 +1,26 @@
-const { createApp } = Vue;
-const { createRouter, createWebHistory } = VueRouter;
+const { createApp } = Vue
+const { createRouter, createWebHistory } = VueRouter
+
+const app = createApp()
 
 app.config.globalProperties = {
-  ...app.config?.globalProperties || {},
-  msgHome: "testHome",
-  msgAbout: "testAbout",
-};
+  ...(app.config?.globalProperties || {}),
+  msgHome: 'testHome',
+  msgAbout: 'testAbout',
+}
 
-const HomePage = { template: `<h1>Home - {{ msgHome }}</h1>` };
-const AboutPage = { template: `<h1>About - {{ msgAbout }}</h1>` };
+const HomePage = { template: `<h1>Home - {{ msgHome }}</h1>` }
+const AboutPage = { template: `<h1>About - {{ msgAbout }}</h1>` }
 
 const routes = [
-  { path: "/", component: HomePage },
-  { path: "/about", component: AboutPage },
-];
+  { path: '/', component: HomePage },
+  { path: '/about', component: AboutPage },
+]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-});
+})
 
-app.use(router);
-app.mount("#app");
+app.use(router)
+app.mount('#app')
